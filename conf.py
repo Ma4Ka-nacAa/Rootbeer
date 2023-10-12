3af5d4ad17fea2caa871ad00968ff25c19c9a663
Ma4Ka nacAa <110987959+Ma4Ka-nacAa@users.noreply.github.com>
2023-10-12 18:59:23 +0300

    The config file
    The config file. Edit this before running the main file! There are instructions inside it don't worry

---
+++ conf.py
@@ -0,0 +1,26 @@
+input('Why did you run this')
+
+### --- [CONFIG FILE] --- ###
+
+# >> !Please enter your bot token here! <<
+TOKEN = 'token'
+
+# The id of the channel the bot will send messages into
+channel_id = 1
+
+# Please don't chage this value! It's required for the bot to work properly
+channel = None # << Please don't chage this value!
+
+# The content of the message the bot will send
+content = 'content'
+
+# The id of the role that will get pinged
+roleId = 1
+
+# Every pingNum'th messageNumber the roleId will be pinged (yea i know i'm bad at descripting stuff but you get it :3)
+pingNum = 250
+
+# Please enter the delay (!) in Minutes (!)
+timeAmount = 60
+
+### --- [CONFIG FILE] --- ###
